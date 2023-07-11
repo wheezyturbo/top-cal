c400095dc2f28330f47b7a102fd8932f7bb4cca5
rubyciide <joelpaul900@gmail.com>
2023-07-11 11:19:02 +0530

    added event listeners for the numbers and operators

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,23 @@ class Calculator{
     mul = (...a)=>a.reduce((b,c)=>b*c);
     div = (a,b)=>a/b;
 }
+
+
+function operate(a,b,c){
+    if(b=='+') return new Calculator().add(a,c);
+    if(b=='-') return new Calculator().sub(a,c);
+    if(b=='*') return new Calculator().mul(a,c);
+    if(b=='/') return new Calculator().div(a,c);
+}
+
+var a='',b='',c='';
+
+
+const numbuttons = document.querySelectorAll('.numbers');
+numbuttons.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        document.getElementById('input').value+=button.textContent;
+        console.log(button.textContent)
+
+    })
+})
